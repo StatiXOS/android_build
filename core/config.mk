5b63443c88b0db102fc34297f785fb0766f88175
Stefen Wakefield <xstefen@gmail.com>
2019-08-03 11:38:03 -0500

    make: we should include some policy in the case that we're doing a real build
    Signed-off-by: Stefen Wakefield <xstefen@gmail.com>
    
    Change-Id: Iede5830093497abe753a34df3bc4913468be39d2

--- core/config.mk
+++ core/config.mk
@@ -1144,6 +1144,12 @@ include $(BUILD_SYSTEM)/ninja_config.mk
 include $(BUILD_SYSTEM)/soong_config.mk
 endif
 
+ifneq ($(STATIX_BUILD),)
+## We need to be sure the global selinux policies are included
+## last, to avoid accidental resetting by device configs
+$(eval include device/statix/sepolicy/common/sepolicy.mk)
+endif
+
 include $(BUILD_SYSTEM)/dumpvar.mk
 # Include any vendor specific config.mk file
 -include vendor/*/build/core/config.mk
