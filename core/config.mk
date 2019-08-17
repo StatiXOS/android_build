f85996e125962509ab87149cc627025cfb00d79a
Anay Wadhera <anay1018@gmail.com>
2019-08-17 14:19:04 -0700

    build: include qcom sepolicy
    Change-Id: I00c5e5a0dca110f05fbe117b2e91a342d509150a

--- core/config.mk
+++ core/config.mk
@@ -1148,6 +1148,7 @@ ifneq ($(STATIX_BUILD),)
 ## We need to be sure the global selinux policies are included
 ## last, to avoid accidental resetting by device configs
 $(eval include device/statix/sepolicy/common/sepolicy.mk)
+$(eval include device/statix/sepolicy/qcom/sepolicy.mk)
 endif
 
 include $(BUILD_SYSTEM)/dumpvar.mk
