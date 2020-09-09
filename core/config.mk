3658a105bae5180a8cdec16d3e8f348379bca88e
Anay Wadhera <awadhera@berkeley.edu>
2020-09-09 10:21:40 -0700

    [DNM] build: switch to if exists include for sepolicy
    * we don't need this for bringup just yet

--- core/config.mk
+++ core/config.mk
@@ -1248,7 +1248,7 @@ DEFAULT_DATA_OUT_MODULES := ltp $(ltp_packages) $(kselftest_modules)
 ifneq ($(STATIX_BUILD),)
 ## We need to be sure the global selinux policies are included
 ## last, to avoid accidental resetting by device configs
-$(eval include device/statix/sepolicy/common/sepolicy.mk)
+$(call inherit-product-if-exists, device/statix/sepolicy/common/sepolicy.mk)
 endif
 
 include $(BUILD_SYSTEM)/dumpvar.mk
