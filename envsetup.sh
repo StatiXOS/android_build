a83c81746a6fed616a2a5fb7b306682bb69500d2
Sourajit Karmakar <sourajit.karmakar@gmail.com>
2021-10-06 15:48:15 -0400

    build: Explicitly call roomservice using python3
    Change-Id: Ie360312f67b47f2d0e3d2c9e35bf20e5b8b92142
    Reviewed-on: https://review.statixos.com/c/android_build/+/5700
    Reviewed-by: Sourajit Karmakar <sourajit@live.com>
    Tested-by: Sourajit Karmakar <sourajit@live.com>

--- envsetup.sh
+++ envsetup.sh
@@ -717,13 +717,13 @@ function lunch()
         # if we can't find the product, try to grab it from our github
         T=$(gettop)
         pushd $T > /dev/null
-        vendor/statix/build/tools/roomservice.py $product
+        python3 vendor/statix/build/tools/roomservice.py $product
         popd > /dev/null
         check_product $product
     else
         T=$(gettop)
         pushd $T > /dev/null
-        vendor/statix/build/tools/roomservice.py $product true
+        python3 vendor/statix/build/tools/roomservice.py $product true
         popd > /dev/null
     fi
     TARGET_PRODUCT=$product \
