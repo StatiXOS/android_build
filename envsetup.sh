964f6c3ea12fae6148ed9c4f8dc58794c86a33d1
Sourajit Karmakar <sourajitk@statixos.com>
2022-12-19 11:20:01 -0500

    build: Remove roomservice
    Reason for revert: https://review.statixos.com/c/android_vendor_statix/+/9144
    
    Squashed commit of the following:
    
    commit 514f32aa1787e7cc2ffe354eec28f858930e73f9
    Author: Sourajit Karmakar <sourajitk@statixos.com>
    Date:   Mon Dec 19 11:19:50 2022 -0500
    
        Revert "make: Add in roomservice"
    
        This reverts commit 61b8ed7520c87e2629330477d907b1b80fad8561.
    
    commit b50a3b968a9ac85d4f656c7b3aabf9d7c0e0fcd7
    Author: Sourajit Karmakar <sourajitk@statixos.com>
    Date:   Mon Dec 19 11:19:43 2022 -0500
    
        Revert "Fix lunch while using roomservice"
    
        This reverts commit 8227ccc685706023094a3de0443effeea1f74653.
    
    commit 82571f5831d8e7a6600185f8a75956603bec1517
    Author: Sourajit Karmakar <sourajitk@statixos.com>
    Date:   Mon Dec 19 11:19:38 2022 -0500
    
        Revert "build: Explicitly call roomservice using python3"
    
        This reverts commit 9856192f69dd5bab51f77351d0e3ea84a9f5c2ad.
    
    Change-Id: Ie8aeeab49501f5d7195413396d68315f768adb37

--- envsetup.sh
+++ envsetup.sh
@@ -769,30 +769,7 @@ function lunch()
     TARGET_BUILD_VARIANT=$variant \
     TARGET_PLATFORM_VERSION=$version \
     build_build_var_cache
-    check_product $product
     if [ $? -ne 0 ]
-    then
-        # if we can't find the product, try to grab it from our github
-        T=$(gettop)
-        python3 vendor/statix/build/tools/roomservice.py $product
-        check_product $product
-    else
-        T=$(gettop)
-        python3 vendor/statix/build/tools/roomservice.py $product true
-    fi
-    TARGET_PRODUCT=$product \
-    TARGET_BUILD_VARIANT=$variant \
-    TARGET_PLATFORM_VERSION=$version \
-    build_build_var_cache
-    if [ $? -ne 0 ]
-    then
-        echo
-        echo "** Don't have a product spec for: '$product'"
-        echo "** Do you have the right repo manifest?"
-        product=
-    fi
-
-    if [ -z "$product" -o -z "$variant" ]
     then
         if [[ "$product" =~ .*_(eng|user|userdebug) ]]
         then
