0db4d7e761e482c8cd15b408f347555228ec8b1e
ralph950412 <ralph950412@gmail.com>
2024-09-07 04:40:17 +0000

    build: Move StatiXOS properties inclusion to core/sysprop_config.mk
    Change-Id: I733dcee789ca2835898381b8f6b1064bb003f3dd

--- core/main.mk
+++ core/main.mk
@@ -178,10 +178,6 @@ endif
 BUILD_WITHOUT_PV := true
 
 # ------------------------------------------------------------
-# Include vendor specific additions to build properties
--include vendor/statix/build/core/props.mk
-
-# ------------------------------------------------------------
 # Define a function that, given a list of module tags, returns
 # non-empty if that module should be installed in /system.
 
